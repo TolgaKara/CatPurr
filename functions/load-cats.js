245f565ec9e497efed67621623cc1a4aeab40b4e
TolgaKara <tolgakara2.0@gmail.com>
2021-05-02 18:05:54 +0200

    Fixed an Error in load cats

--- functions/load-cats.js
+++ functions/load-cats.js
@@ -1,7 +1,34 @@
 const fetch = require("node-fetch");
+const { hasuraRequest } = require("./util/hasura");
 
 exports.handler = async () => {
 	const cats = await fetch("https://api.thecatapi.com/v1/breeds").then((res) => res.json());
+
+	const hasuraPromise = hasuraRequest({
+		query: `
+      mutation InsertOrUpdateBoops($cats: [boops_insert_input!]!) {
+        boops: insert_boops(objects: $cats, on_conflict: {constraint: boops_pkey, update_columns: id}) {
+          returning {
+            id
+            count
+          }
+        }
+      }
+    `,
+		variables: {
+			cats: cats.map(({ id }) => ({ id, count: 0 })),
+		},
+	});
+
+	const [hasuraData] = await Promise.all([hasuraPromise]);
+
+	const completeData = cats.map((cat) => {
+		const boops = hasuraData.boops.returning.find((b) => b.id === cat.id);
+		return {
+			...cat,
+			boops: boops.count,
+		};
+	});
 
 	// const cats = [];
 	return {
@@ -9,6 +36,6 @@ exports.handler = async () => {
 		headers: {
 			"Content-Type": "application/json",
 		},
-		body: JSON.stringify(cats),
+		body: JSON.stringify(completeData),
 	};
 };
