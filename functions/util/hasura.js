e019e687fa1585a0e54e671ff044fbd7dcbdc2d6
TolgaKara <tolgakara2.0@gmail.com>
2021-05-02 16:18:29 +0200

    Sending Graph request to Hasura

---
+++ functions/util/hasura.js
@@ -0,0 +1,19 @@
+const fetch = require("node-fetch");
+
+exports.hasuraRequest => async function({query, variables}) {
+  const result = await fetch(process.env.HASURA_URL, {
+    method: 'POST'
+    headers: {
+      'X-Hasura-Admin-Secret': process.env.SECRET_ADMIN
+    },
+    body: JSON.stringify({query, variables})
+  }).then(res => res.json())
+
+  if (!result || !result.data) {
+    console.error(result)
+    return []
+  }
+
+  return result.data;
+
+}
